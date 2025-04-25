5da01a12f0f896e20a39f22fdfcb533df3509972
Derciel <dercieljr2@gmail.com>
2025-04-25 14:52:41 -0300

    atualização no node e vite

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: './',
-  resolve: {
-    alias: {
-      'axios': 'axios/dist/axios.js'
-    }
-  }
 });
