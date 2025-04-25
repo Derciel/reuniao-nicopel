3e004831cb918cc77e5269a3e3dfd44ad0522250
Derciel <dercieljr2@gmail.com>
2025-04-25 15:20:51 -0300

    atualização no node_modules

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default defineConfig({
-  plugins: [react()],
-  base: './',
+  plugins: [
+    react(),
+    commonjs({
+      include: /node_modules/,
+    })
+  ],
+  resolve: {
+    alias: {
+      './env/data.js': 'axios/lib/env/data.js'
+    }
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
+  }
 });
