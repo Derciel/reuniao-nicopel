af413dcaab2ddefd9d2895f1150ff5098a739ded
Derciel <dercieljr2@gmail.com>
2025-04-28 08:09:20 -0300

    atualização geral

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,13 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import commonjs from 'vite-plugin-commonjs';
 
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    include: ['axios'], 
-  },
+  plugins: [react(), commonjs()],
   build: {
     target: 'esnext',
     commonjsOptions: {
-      include: [/node_modules/], 
-    },
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
+      transformMixedEsModules: true,
     },
   },
 });
